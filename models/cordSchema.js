0652ba7043a403eada5dfa75aeaaec6a895cb774
Mohit sen <mohitsen0102@gmail.com>
2023-10-22 03:16:21 +0530

    Update cordSchema.js

--- models/cordSchema.js
+++ models/cordSchema.js
@@ -43,7 +43,69 @@ const stateCordSchema = new Schema({
     }
 });
 
+const stateCordSchema = new Schema({
+
+    state : {
+        type : String,
+        require : true
+    },
+
+    lat : {
+        type : Number,
+        require : true
+    },
+
+    long : {
+        type : Number,
+        require : true
+    }
+});
+
+const boundSchema = new Schema({
+
+    city : {
+        type : String,
+        require : true
+    },
+
+    state : {
+        type : String,
+        require : true
+    },
+
+    lat : {
+        type : Number,
+        require : true
+    },
+
+    long : {
+        type : Number,
+        require : true
+    },
+    
+    s : {
+        type : Number,
+        require : true
+    },
+
+    w : {
+        type : Number,
+        require : true
+    },
+
+    n : {
+        type : Number,
+        require : true
+    },
+
+    e : {
+        type : Number,
+        require : true
+    }
+});
+
 const cityCord = mongoose.model('cityCordData' , cityCordSchema);
 const stateCord = mongoose.model('stateCordData' , stateCordSchema);
+const boundCord = mongoose.model('boundCordData' , boundSchema);
 
-module.exports = {cityCord , stateCord};
+module.exports = {cityCord , stateCord , boundCord};
