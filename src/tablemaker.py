bb315545cafe1e6ad3a8660a00f16ca346e5e40e
PedroManuelAtienzaHuerta <pedromanuelatienzahuerta@gmail.com>
2025-01-26 14:18:08 +0100

    Update tablemaker.py
    The code is simplified for clarity.
    
    Optimizing the calculation of the maximum length:
    The calculation of the maximum length can be optimized using list compression.

--- src/tablemaker.py
+++ src/tablemaker.py
@@ -1,29 +1,15 @@
+def tablemaker(data):  # OPTIMIZED VARIANT
+    l = len(data[0])
+    lf = [max(len(str(i[p])) for i in data) for p in range(l)]
 
-#credits to @Asvin1 for this pretty tablemaker function
-#link to project: https://github.com/asvin1/cli-table
-
-###############################
-
-def tablemaker(data):     #OPTIMIZED VARIANT
-    l=len(data[0])
-    lf=[]
-    for p in range(l):
-        li=[]
-        mx=0
-        for i in data:
-            aa=len(str(i[p]))
-            li.append(aa)
-            mx=max(li)
-        else:
-            lf.append(mx)
-    print("+","-"*((len(lf)*4)+sum(lf)-2),"+",sep="")
+    print("+" + "-" * ((len(lf) * 4) + sum(lf) - 2) + "+")
     for p in data:
         for i in range(l):
-            print("|"," ",p[i]," "," "*(lf[i]-len(str(p[i]))),"|",end="",sep="")
-        if data[0]==p:
+            print("|", " ", p[i], " " * (lf[i] - len(str(p[i]))), "|", end="", sep="")
+        if data[0] == p:
             print()
-            print("+","-"*((len(lf)*4)+sum(lf)-2),"+",sep="",end="")            
+            print("+" + "-" * ((len(lf) * 4) + sum(lf) - 2) + "+", end="")
         print()
-    print("+","-"*((len(lf)*4)+sum(lf)-2),"+",sep="")# table making end
-    
-###############################
+    print("+" + "-" * ((len(lf) * 4) + sum(lf) - 2) + "+")  # table making end
+
+
